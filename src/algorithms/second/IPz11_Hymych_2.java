c56f6fa1463f489d25f3b829acf108b84acedc80
vovik541 <conanzhill@gmail.com>
2022-05-01 15:34:01 +0300

    Java algorithms. Lab 2  algorithm refactored

--- src/algorithms/second/IPz11_Hymych_2.java
+++ src/algorithms/second/IPz11_Hymych_2.java
@@ -7,13 +7,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IPz11_Hymych_2 {
+    private static HashMap<Integer, Integer> sortedIndexes;
 
     public static void main(String[] args) {
         String inputFilePath = readInputFileFullPath();
         int x = readCustomer();
         Map<Integer, ArrayList<Integer>> input = readFromFile(inputFilePath);
-        HashMap<Integer, Integer> preferences = arrToMap(input.get(x));
-        HashMap<Integer, Integer> result = lookForInversions(x, input, preferences);
+        sortedIndexes = arrToMap(input.get(x));
+        HashMap<Integer, Integer> result = lookForInversions(x, input);
         LinkedHashMap<Integer, Integer> sorted = sortByValue(result.entrySet());
         createFile(sorted, x);
     }
@@ -26,48 +27,32 @@ public class IPz11_Hymych_2 {
                         (e1, e2) -> e1, LinkedHashMap::new));
     }
 
-    private static void createFile(LinkedHashMap<Integer, Integer> result, int x) {
-        File file = new File("IPz11_hymych_02_output.txt");
-
-        try (FileWriter writer = new FileWriter(file, false)) {
-            writer.write(String.format("%d\n", x));
-            for (Entry<Integer, Integer> entry : result.entrySet()) {
-                writer.write(String.format("%d %d\n", entry.getKey(), entry.getValue()));
-            }
-
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    private static HashMap<Integer, Integer> lookForInversions(int x, Map<Integer, ArrayList<Integer>> input, HashMap<Integer, Integer> preferences) {
+    private static HashMap<Integer, Integer> lookForInversions(int x, Map<Integer, ArrayList<Integer>> input) {
         return new HashMap<>() {{
-            for (Map.Entry<Integer, ArrayList<Integer>> entry : input.entrySet()) {
-                if (entry.getKey() != x) {
-                    this.put(entry.getKey(), findInversion(entry.getValue(), preferences, 0));
-                }
+            for (Entry<Integer, ArrayList<Integer>> inputEntry : input.entrySet()) {
+                this.put(inputEntry.getKey(), findInversion(inputEntry.getValue(), 0));
             }
+            this.remove(x);
         }};
     }
 
-    private static int findInversion(ArrayList<Integer> toCompare, HashMap<Integer, Integer> preferences, int deep) {
+    private static int findInversion(ArrayList<Integer> toCompare, int deep) {
         if (deep == toCompare.size() - 1) {
             return 0;
         }
         int numOfInversions = 0;
         for (int i = deep; i < toCompare.size() - 1; i++) {
-            if (preferences.get(toCompare.get(deep)) > preferences.get(toCompare.get(i + 1))) {
+            if (toCompare.get(sortedIndexes.get(deep)) > toCompare.get(sortedIndexes.get(i + 1))) {
                 numOfInversions++;
             }
         }
-        return numOfInversions + findInversion(toCompare, preferences, deep + 1);
+        return numOfInversions + findInversion(toCompare, deep + 1);
     }
 
     private static HashMap<Integer, Integer> arrToMap(ArrayList<Integer> prefsArr) {
         return new HashMap<>() {{
             for (int i = 0; i < prefsArr.size(); i++) {
-                this.put(prefsArr.get(i), i);
+                this.put(prefsArr.get(i) - 1, i);
             }
         }};
     }
@@ -104,5 +89,20 @@ public class IPz11_Hymych_2 {
             throw new RuntimeException(e);
         }
         return input;
+    }
+
+    private static void createFile(LinkedHashMap<Integer, Integer> result, int x) {
+        File file = new File("IPz11_hymych_02_output.txt");
+
+        try (FileWriter writer = new FileWriter(file, false)) {
+            writer.write(String.format("%d\n", x));
+            for (Entry<Integer, Integer> entry : result.entrySet()) {
+                writer.write(String.format("%d %d\n", entry.getKey(), entry.getValue()));
+            }
+
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
